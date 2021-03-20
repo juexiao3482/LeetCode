b85fbcbfa56c5411d99d143f1162af9053666235
wangruiyi <juexiao3482@163.com>
2021-03-20 17:31:52 +0800

    2021.3.20

---
+++ 3.无重复字符的最长子串.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=javascript
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// 滑动窗口，依次判断当前字符是否在窗口中，有则删除窗口中该字符之前的字符串
+// 然后记录max值
+// s[i]就可以遍历字符串中每一个字符
+var lengthOfLongestSubstring = function(s) {
+    // 注意声明的是数组
+    // push进入的时候回以字符存入，即abc=：>"a","b","c"
+    let str=[];
+    max=0;
+    for(let i=0;i<s.length;i++){
+        // indexOf返回该字符在字符串中的下标，没有返回-1，
+        // 也可以设置第二个参数，代表从第几个字符开始找
+        // 输入字符串参数，则是查找子串
+        // 空串的长度是0，之间跳出for循环
+        let index = str.indexOf(s[i]);
+        // 如果s[i]里存在该字符
+        if(index !==-1){
+            // 删除从0到index+1的数组，会删除到下标为index的。
+            // eg. (0,1)删除下标为0的
+            str.splice(0,index+1)
+        }
+        // push把元素加入数组，末位追加
+        // charAt(i)返回字符串中的第i个字符
+        str.push(s.charAt(i));
+        max = Math.max(str.length, max);
+    }
+    return max;
+};
+// @lc code=end
+
--- 7.整数反转.js
+++ 7.整数反转.js
@@ -13,16 +13,17 @@ var reverse = function(x) {
     // let作用域是块内
     let res = 0
     while(x){
-        /* 个位要变成最高位，因此需要res成10，这样有多少
+        /* 个位要变成最高位，因此需要res乘10，这样有多少
          位就乘了多少倍 */
          res = res * 10 + x % 10;
         //  pow是底数，几次方
         if (res>Math.pow(2,31)-1||res<Math.pow(-2,31)){
              return 0;
          }
-        // ~~可以将数字类型的转换成纯字符串，有数字以外的一律输出0，bool
-        // 类型的true0，false1，undefined=0，！undefined=1
+        // ~~可以将其他类型的转换成纯数字，有数字以外的一律输出0，还可以去除小数。
+        // bool类型的true0，false1，undefined=0，！undefined=1
         x=~~(x/10)
+        // 主要是去除小数
     }
     return res;
 
